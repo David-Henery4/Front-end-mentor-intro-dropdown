cd837792c7ba61f035bf094cd56365cf10bc4de2
DizzlyD <david4henery00@hotmail.com>
2022-05-06 23:58:34 +0100

    close-mob-nav

--- J.S/main.js
+++ J.S/main.js
@@ -5,7 +5,15 @@ const navSlideContainer = document.querySelector(".nav-slide");
 const burger = document.querySelector(".burger-icon");
 const navMobCloseIcon = document.querySelector(".nav-slide-close");
 
+const removeNav = () => {
+  navMobContainer.classList.remove("nav-mob-container--active");
+  navSlideContainer.classList.remove("nav-slide--active");
+}
 
+navMobContainer.addEventListener("click", function(e){
+const clicked = e.target.classList[0] === "nav-mob-container"
+  if (clicked) removeNav()
+})
 
 burger.addEventListener("click", function(e){
     navMobContainer.classList.add("nav-mob-container--active");
@@ -13,9 +21,9 @@ burger.addEventListener("click", function(e){
 });
 
 navMobCloseIcon.addEventListener("click", function(e){
-    navMobContainer.classList.remove("nav-mob-container--active");
-    navSlideContainer.classList.remove("nav-slide--active");
+  removeNav()
 })
+
 
 // const mobNavAni = () => {
     
